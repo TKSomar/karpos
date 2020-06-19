15a4a1b792100229b73a68a98a56c62fcc892c6e
TKSomar <tksomar@gmail.com>
2020-06-19 13:51:17 -0500

    Working to finish post form

---
+++ src/Components/Post/Post.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Post extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            img: '',
+            content: '',
+            loading: true
+        }
+    }
+
+    submit() {
+        axios.post('/api/post', this.state)
+        .then(res => this.props.history.push('/dashboard'))
+        .catch(err => alert('Make sure you are logged in to create a new post.'))
+    }
+
+    render() {
+        return (
+            <div className="Post">
+                Create a new post.
+            </div>
+        )
+    }
+}
+
+export default Post;
