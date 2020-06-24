e39db2f95c9eafa86465b33f480c5b677dd8508b
TKSomar <tksomar@gmail.com>
2020-06-24 14:15:01 -0500

    Register view styles complete, still needs responsiveness

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -13,7 +13,39 @@ class Register extends Component {
     render() {
         return (
             <div className="Register">
-            This is the registration component.
+
+                <div className="logo_container">
+                    <h1 className="register_logo">Karpós</h1>
+                    <p>The fruit discovery app.</p>
+                </div>
+
+                <div className="form_container">
+
+                    <div className="register_input_box">
+                        <p>First name</p>
+                        <input type="text" id="first_name"></input>
+                    </div>
+
+                    <div className="register_input_box">
+                        <p>Last name</p>
+                        <input type="text" id="last_name"></input>
+                    </div>
+
+                    <div className="register_input_box">
+                        <p>Email</p>
+                        <input type="text" id="email"></input>
+                    </div>
+
+                    <div className="register_input_box">
+                        <p>Password</p>
+                        <input type="text" id="password"></input>
+                    </div>
+
+                    <div className="register_button_container">
+                        <button className="register_btn">Create account</button>
+                    </div>
+
+                </div>
             </div>
         )
     }
